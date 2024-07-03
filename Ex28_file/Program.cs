fabdf1262cb46453ef5fb912bd30506d045dd944
dangtiendat24 <148854633+dangtiendat24@users.noreply.github.com>
2024-07-03 21:32:38 +0700

    Add files via upload

---
+++ Ex28_file/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            class_demo ob = new class_demo();
+            ob.Show();
+
+            Console.WriteLine("Nhập n:");
+            ushort n = ob.nhapsonguyen2bytekhongdau();
+            Console.WriteLine("n=" + n.ToString());
+        }
+    }
+}
