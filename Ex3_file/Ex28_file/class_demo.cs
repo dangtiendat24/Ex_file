afbd78c57a30c3cddb4f9332b25601f3b556eb91
dangtiendat24 <148854633+dangtiendat24@users.noreply.github.com>
2024-07-03 21:29:49 +0700

    Add files via upload

---
+++ Ex3_file/Ex28_file/class_demo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex4
+{
+    public class class_demo
+    {
+        public void Show()
+        {
+            Console.WriteLine("Tôi là một lớp đơn giản!");
+        }
+
+        public ushort nhapsonguyen2bytekhongdau()
+        {
+            ushort n = 0;
+            while (true)
+            {
+                try
+                {
+                    string sz;
+                    sz = Console.ReadLine();
+                    n = ushort.Parse(sz);
+                    break;
+                }
+                catch
+                {
+                    Console.Beep();
+                    //Console.WriteLine("...");
+                }
+            }
+            return n;
+        }
+    }
+}
